ed14ddc80cc7666cc42027b55991b2c6f3d4bbe7
lukemorse <luke.morse@gmail.com>
2017-10-11 13:24:51 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import faceShot from './JLu Head shot.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={faceShot} className="App-logo" alt="logo" />
+          <h1 className="App-title">Luke Morse</h1>
+          <span className="Links">
+            <h2>
+              <a href="#">Music</a>
+            </h2>
+            <h2>
+              <a href="#">Development</a>
+            </h2>
+            <h2>
+              <a href="#">Bio</a>
+            </h2>
+            <h2>
+              <a href="#">Pizza</a>
+            </h2>
+          </span>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
