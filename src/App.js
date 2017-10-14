8ed98fd3b44836d8b2654e03174708f065408589
lukemorse <luke.morse@gmail.com>
2017-10-13 21:26:57 -0500

    Actually added Background Files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import faceShot from './JLu Head shot.png';
 import './App.css';
+import Background from './Background.js';
 
 class App extends Component {
   render() {
@@ -8,19 +9,26 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={faceShot} className="App-logo" alt="logo" />
-          <h1 className="App-title">Luke Morse</h1>
-          <span className="Links">
-            <h2>
+          <span className="Menu">
+            <h3>
               <a href="#">Development</a>
-            </h2>
-            <h2>
+            </h3>
+            <h3>
               <a href="#">Music</a>
-            </h2>
-            <h2>
+            </h3>
+            <h3>
               <a href="#">Sound</a>
+            </h3>
+            <h2>
+              Hello! I'm Luke Morse, a well-rounded front-end software engineer, composer, and sound-designer. Send me
+              electronic mail:{' '}
+              <a class="Email" href="mailto:luke.morse@gmail.com">
+                luke.morse@gmail.com
+              </a>
             </h2>
           </span>
         </header>
+        <Background />
       </div>
     );
   }
---
+++ src/Background.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import './Background.css';
+
+export class Background extends Component {
+  render() {
+    return <h1>BACKGROUND</h1>;
+  }
+}
