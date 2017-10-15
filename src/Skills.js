e27ae4bc2fdb74d0b770bf409f815abdc6b9adf7
lukemorse <luke.morse@gmail.com>
2017-10-15 13:59:51 -0500

    Create Skills files

---
+++ src/Skills.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './Skills.css';
+
+export default class Background extends Component {
+  render() {
+    return (
+      <div className="skills">
+        <h1 className="section_title">BACKGROUND</h1>
+        <div className="section_content">
+          <h3>
+            I started programming in graduate school, where I wrote sound-synthesis programs using SuperCollider. After
+            earning my Master's of Music in Composition, I began studying iOS development. I released my first app, a
+            game called Ice Cream Explosion, in the App Store in February, 2017. Since then, I have been working with
+            two Chicago startups, Jupiter 69 and Adapted Actualization Labs. <br />
+            <br />Coming into front-end development from a background in composition, I enjoy applying ratios of small
+            integers to user interfaces and visual design.
+          </h3>
+        </div>
+      </div>
+    );
+  }
+}
