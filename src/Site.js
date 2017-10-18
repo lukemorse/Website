006585e16dff727988d905b5bd2e00ef271e2203
lukemorse <luke.morse@gmail.com>
2017-10-18 17:56:46 -0500

    Add react-animate-on-scroll

--- src/Site.js
+++ src/Site.js
@@ -3,46 +3,48 @@ import headShot from './JLu Head shot.png';
 import './Site.css';
 import Background from './Background.js';
 import Skills from './Skills.js';
-import ScrollReveal from 'scrollreveal';
+import ScrollAnimation from 'react-animate-on-scroll';
+import 'animate.css/animate.min.css';
 
 class Site extends Component {
-  componentDidMount() {
-    window.sr = ScrollReveal({duration: 2000});
-    window.sr.reveal('.Site');
-  }
-
   render() {
     return (
       <div>
         <div className="Site">
-          <header className="Header">
-            <img src={headShot} className="Logo" alt="logo" />
-            <span className="Menu">
-              <h3>
-                <a href="#">Development</a>
-              </h3>
-              <h3>
-                <a href="#">Music</a>
-              </h3>
-              <h3>
-                <a href="#">Sound</a>
-              </h3>
-            </span>
-          </header>
-          <div className="Intro">
-            <h2>
-              Hello! I'm Luke Morse, a well-rounded front-end software engineer, composer, and sound-designer. Send me
-              electronic mail:{' '}
-              <a className="Email" href="mailto:luke.morse@gmail.com">
-                luke.morse@gmail.com
-              </a>
-            </h2>
-          </div>
+          <ScrollAnimation animateIn="fadeIn">
+            <header className="Header">
+              <img src={headShot} className="Logo" alt="logo" />
+              <span className="Menu">
+                <h3>
+                  <a href="#">Development</a>
+                </h3>
+                <h3>
+                  <a href="#">Music</a>
+                </h3>
+                <h3>
+                  <a href="#">Sound</a>
+                </h3>
+              </span>
+            </header>
+          </ScrollAnimation>
+          <ScrollAnimation animateIn="fadeIn">
+            <div className="Intro">
+              <h2>
+                Hello! I'm Luke Morse, a well-rounded front-end software engineer, composer, and sound-designer. Send me
+                electronic mail:{' '}
+                <a className="Email" href="mailto:luke.morse@gmail.com">
+                  luke.morse@gmail.com
+                </a>
+              </h2>
+            </div>
+          </ScrollAnimation>
         </div>
-        <div>
+        <ScrollAnimation animateIn="fadeIn">
           <Background />
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeIn">
           <Skills />
-        </div>
+        </ScrollAnimation>
       </div>
     );
   }
