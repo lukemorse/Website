f27c8f068a42fd746de5ca4cc0332d6a4c568aa4
lukemorse <luke.morse@gmail.com>
2017-10-20 16:25:51 -0500

    Fixed links in Projects

--- src/Projects.js
+++ src/Projects.js
@@ -10,10 +10,13 @@ export default class Projects extends Component {
         <div className="content">
           <h1 className="section_title">PROJECTS</h1>
           <div className="section_content">
-            <img src={iceCreamExp} className="IceCream" alt="Ice Cream Explosion!" />
+            <a href="https://itunes.apple.com/us/app/ice-cream-explosion/id1205754650?mt=8">
+              <img className="IceCream" src={iceCreamExp} alt="Ice Cream Explosion!" />
+            </a>
             <h3 className="Explanation">
-              Ice Cream Explosion is the first app I released to the App Store. It is an original arcade style game that
-              I created the music and sound FX for.
+              <a href="https://itunes.apple.com/us/app/ice-cream-explosion/id1205754650?mt=8">Ice Cream Explosion</a> is
+              the first app I released to the App Store. It is an original arcade style game that I created the{' '}
+              <a href="https://soundcloud.com/lukemorse/sets/ice-cream-explosion-soundtrack">music</a> and sound FX for.
             </h3>
           </div>
         </div>
