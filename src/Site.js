870d81f92103a35b45881c0f4d25dfe725eae8ef
lukemorse <luke.morse@gmail.com>
2017-10-20 15:12:37 -0500

    Add Projects files

--- src/Site.js
+++ src/Site.js
@@ -25,7 +25,7 @@ class Site extends Component {
               </h3>
             </span>
           </header>
-          <ScrollAnimation animateIn="fadeInUp">
+          <ScrollAnimation animateIn="fadeInUp" animateOnce="true">
             <div className="Intro">
               <h2>
                 Hello! I'm Luke Morse, a well-rounded front-end software engineer, composer, and sound-designer. Send me
@@ -37,10 +37,13 @@ class Site extends Component {
             </div>
           </ScrollAnimation>
         </div>
-        <ScrollAnimation animateIn="fadeInUp">
+        <ScrollAnimation animateIn="fadeInUp" animateOnce="true">
           <Background />
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp">
+        <ScrollAnimation animateIn="fadeInUp" animateOnce="true">
+          <Skills />
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInUp" animateOnce="true">
           <Skills />
         </ScrollAnimation>
       </div>
--- src/Skills.js
+++ src/Skills.js
@@ -1,44 +1,13 @@
 import React, {Component} from 'react';
-import './Skills.css';
+import './Projects.css';
 import './Generic_Content.css';
 
-export default class Skills extends Component {
+export default class Projects extends Component {
   render() {
     return (
       <div className="content">
-        <h1 className="section_title">SKILLS</h1>
-        <div className="section_content">
-          <div className="skills_category">
-            <div className="skills_category_title">LANGUAGES</div>
-            <ul>
-              <li>Swift</li>
-              <li>Objective-C</li>
-              <li>JavaScript</li>
-              <li>HTML5/CSS3</li>
-              <li>C++</li>
-            </ul>
-          </div>
-          <div className="skills_category">
-            <div className="skills_category_title">FRAMEWORKS</div>
-            <ul>
-              <li>React</li>
-              <li>UIKit</li>
-              <li>XCTest</li>
-              <li>SpriteKit</li>
-              <li>Core Data</li>
-            </ul>
-          </div>
-          <div className="skills_category">
-            <div className="skills_category_title">TOOLS</div>
-            <ul>
-              <li>Git & Github</li>
-              <li>CocoaPods</li>
-              <li>Command Line</li>
-              <li>Chrome DevTools</li>
-              <li>Postman</li>
-            </ul>
-          </div>
-        </div>
+        <h1 className="section_title">PROJECTS</h1>
+        <div className="section_content" />
       </div>
     );
   }
