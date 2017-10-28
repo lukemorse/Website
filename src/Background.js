0f7167b58c2d776b6bc9b9cbca00483db350bd0c
lukemorse <luke.morse@gmail.com>
2017-10-28 13:37:38 -0500

    Fix package.json

--- src/Background.js
+++ src/Background.js
@@ -9,11 +9,11 @@ export default class Background extends Component {
         <div className="section_content">
           <h3>
             I started programming in graduate school, where I wrote sound-synthesis programs using{' '}
-            <a href="http://supercollider.github.io/SuperCollider" target="_blank">
+            <a href="http://supercollider.github.io/SuperCollider" target="_blank" rel="noopener noreferrer">
               SuperCollider
             </a>. After earning my Master's of Music in Composition, I began studying iOS development. I released my
             first app, a game called{' '}
-            <a href="https://itunes.apple.com/us/app/ice-cream-explosion/id1205754650?mt=8" target="_blank">
+            <a href="https://itunes.apple.com/us/app/ice-cream-explosion/id1205754650?mt=8" target="_blank" rel="noopener noreferrer">
               Ice Cream Explosion
             </a>, in the App Store in February, 2017. Since then, I have been working with two Chicago startups, Jupiter
             69 and Adapted Actualization Labs. <br />
