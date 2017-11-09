aa419acfa9b9c8c0ed1c77da5610f34fe1e5cad5
lukemorse <luke.morse@gmail.com>
2017-11-09 14:46:36 -0600

    Rid bg.css, fix spacing

--- src/Background.js
+++ src/Background.js
@@ -6,14 +6,17 @@ export default class Background extends Component {
     return (
       <div className="content">
         <h1 className="section_title">BACKGROUND</h1>
-        <div className="section_content">
+        <div className="section_content" id="background">
           <h3>
             I started programming in graduate school, where I wrote sound-synthesis programs using{' '}
             <a href="http://supercollider.github.io/SuperCollider" target="_blank" rel="noopener noreferrer">
               SuperCollider
             </a>. After earning my Master's of Music in Composition, I began studying iOS development. I released my
             first app, a game called{' '}
-            <a href="https://itunes.apple.com/us/app/ice-cream-explosion/id1205754650?mt=8" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://itunes.apple.com/us/app/ice-cream-explosion/id1205754650?mt=8"
+              target="_blank"
+              rel="noopener noreferrer">
               Ice Cream Explosion
             </a>, in the App Store in February, 2017. Since then, I have been working with two Chicago startups, Jupiter
             69 and Adapted Actualization Labs. <br />
